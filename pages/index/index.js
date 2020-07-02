e915ab986ce4c113bd50dd90d1446a0721d5e63f
王震 <lavalike@yeah.net>
2020-07-02 17:09:59 +0800

    完成首页开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,154 @@
 //index.js
+var util = require('../../utils/util')
 Page({
   data: {
-
+    header_image_url : './images/ic_header.png',
+    header_content : '',
+    header_note : '',
+    current_day : new Date().getDate(),
+    lunar_calendar : '',
+    lunar_des : '',
+    lunar_detail : '',
+    taboo_text_avoid : '',
+    taboo_text_suit : '',
   },
   onLoad: function () {
-
-  }
+    this.init()
+    this.requestHeaderInfo()
+    this.requestLunarInfo()
+  },
+  /**
+   * 初始化本地缓存
+   */
+  init(){
+    // 每日一句
+    var header_url = wx.getStorageSync('header_image_url')
+    if(header_url){
+      this.setData({
+        header_image_url : header_url
+      })
+    }
+    var content = wx.getStorageSync('header_content')
+    if(content){
+      this.setData({
+        header_content : content
+      })
+    }
+    var note = wx.getStorageSync('header_note')
+    if(note){
+      this.setData({
+        header_note : note
+      })
+    }
+    // 农历
+    var lunarCalendar =wx.getStorageSync('lunarCalendar')
+    if(lunarCalendar){
+      this.setData({
+        lunar_calendar : lunarCalendar
+      })
+    }
+    var lunarDes =wx.getStorageSync('lunarDes')
+    if(lunarDes){
+      this.setData({
+        lunar_des : lunarDes
+      })
+    }
+    var lunarDetail =wx.getStorageSync('lunarDetail')
+    if(lunarDetail){
+      this.setData({
+        lunar_detail : lunarDetail
+      })
+    }
+    var avoid =wx.getStorageSync('avoid')
+    if(avoid){
+      this.setData({
+        taboo_text_avoid : avoid
+      })
+    }
+    var suit =wx.getStorageSync('suit')
+    if(suit){
+      this.setData({
+        taboo_text_suit : suit
+      })
+    }
+  },
+  /**
+   * 请求每日一句
+   */
+  requestHeaderInfo(){
+    const that = this
+    wx.request({
+      url: 'https://open.iciba.com/dsapi/',
+      method : 'GET',
+      success (res) {
+        var picture = res.data.picture2;
+        var content = res.data.content
+        var note = res.data.note;
+        that.saveHeaderInfo(picture,content,note)
+        that.setData({
+          header_image_url : picture,
+          header_content : content,
+          header_note : note
+        })
+      },fail (res) {
+        console.log('error : '+ JSON.stringify(res))
+      },
+    })
+  },
+  /**
+   * 缓存每日一句
+   * @param {*} picture 
+   * @param {*} content 
+   * @param {*} note 
+   */
+  saveHeaderInfo : function(picture,content,note){
+    wx.setStorageSync('header_image_url', picture)
+    wx.setStorageSync('header_content', content)
+    wx.setStorageSync('header_note', note)
+  },
+  /**
+   * 请求农历详情
+   */
+  requestLunarInfo(){
+    const that = this
+    var param = util.formatTime('yyyyMMdd', new Date())
+    wx.request({
+      url: 'https://www.mxnzp.com/api/holiday/single/' + param,
+      method : 'GET',
+      header : {
+        'app_id' : 'moehknojlpi9llvn',
+        'app_secret' : 'aVBrU2pBSTJFY3c3aUZVMjZMbW1Wdz09',
+      },
+      success (res) {
+        that.saveLunarInfo(res.data.data)
+        that.init()
+      },
+      fail (res) {
+        console.log(JSON.stringify(res))
+      },
+    })
+  },
+  /**
+   * 缓存农历详情
+   * @param {*} data 
+   */
+  saveLunarInfo(data){
+    var weekdaycn = util.weekdaycn(data.weekDay)
+    var chineseZodiac = data.chineseZodiac
+    var yearTips = data.yearTips
+    var lunarCalendar = data.lunarCalendar
+    var lunarDes = util.formatTime('yyyy年MM月dd日',new Date()) + ' ' + data.typeDes
+    var solarTerms = data.solarTerms
+    var avoid = data.avoid
+    var suit = data.suit
+    var dayOfYear = data.dayOfYear
+    var weekOfYear = data.weekOfYear
+    var lunarDetail = weekdaycn +' ' + yearTips + '[' + chineseZodiac + ']年' + ' ' + solarTerms + '\n' + util.formatTime('yyyy', new Date()) + '年第' +dayOfYear + '天、第'+ weekOfYear +'周'
+    //缓存本地
+    wx.setStorageSync('lunarCalendar', lunarCalendar)
+    wx.setStorageSync('lunarDes', lunarDes)
+    wx.setStorageSync('lunarDetail', lunarDetail)
+    wx.setStorageSync('avoid', avoid)
+    wx.setStorageSync('suit', suit)
+  },
 })
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,44 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+function weekdaycn(weekday){
+  switch(weekday){
+    case 1:
+      return "星期一";
+    case 2:
+      return "星期二";
+    case 3:
+      return "星期三";
+    case 4:
+      return "星期四";
+    case 5:
+      return "星期五";
+    case 6:
+      return "星期六";
+    case 7:
+      return "星期日";
+    default:
+      return "";
+  }
 }
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+function formatTime(fmt, date) {
+  let ret;
+  const opt = {
+      "y+": date.getFullYear().toString(),        // 年
+      "M+": (date.getMonth() + 1).toString(),     // 月
+      "d+": date.getDate().toString(),            // 日
+      "H+": date.getHours().toString(),           // 时
+      "m+": date.getMinutes().toString(),         // 分
+      "s+": date.getSeconds().toString()          // 秒
+      // 有其他格式化字符需求可以继续添加，必须转化成字符串
+  };
+  for (let k in opt) {
+      ret = new RegExp("(" + k + ")").exec(fmt);
+      if (ret) {
+          fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+      };
+  };
+  return fmt;
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime : formatTime,
+  weekdaycn : weekdaycn,
 }
