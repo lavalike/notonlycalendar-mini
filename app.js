3f2f7b782a7bb93f761d01ff5859fb28026b761b
王震 <lavalike@yeah.net>
2020-07-03 18:51:43 +0800

    支持在线更新

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    this.checkNewVersion()
+  },
+  checkNewVersion(){
+    if(wx.canIUse('getUpdateManager')){
+      const manager = wx.getUpdateManager()
+      manager.onCheckForUpdate(function (res) {
+        if(res.hasUpdate){
+          manager.onUpdateReady(function(){
+            wx.showModal({
+              content : '新版本已经准备好，是否更新?',
+              success (res) {
+                if(res.confirm){
+                  manager.applyUpdate()
+                }
+              }
+            })
+          })
+        }
+      })
+    }else{
+      wx.showModal({
+        content: "当前微信版本过低，请升级到最新微信版本后重试"
+      })
+    }
   },
 })
